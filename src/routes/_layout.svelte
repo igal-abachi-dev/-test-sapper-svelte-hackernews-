1d4ac2668623784ae9504aa19469c414cc638004
hila-abachi <igalk474@gmail.com>
2020-09-18 05:49:31 +0300

    .

---
+++ src/routes/_layout.svelte
@@ -0,0 +1,32 @@
+<script>
+	import { stores } from '@sapper/app';
+	import Nav from '../components/Nav.svelte';
+	import PreloadingIndicator from '../components/PreloadingIndicator.svelte';
+	import ThemeToggler from '../components/ThemeToggler.svelte';
+
+	export let segment;
+
+	const { preloading } = stores();
+</script>
+
+<style>
+	main {
+		position: relative;
+		max-width: 56em;
+		padding: 2em;
+		margin: 0 auto;
+		box-sizing: border-box;
+	}
+</style>
+
+<Nav {segment}/>
+
+{#if $preloading}
+	<PreloadingIndicator/>
+{/if}
+
+<main>
+	<slot></slot>
+</main>
+
+<ThemeToggler/>
